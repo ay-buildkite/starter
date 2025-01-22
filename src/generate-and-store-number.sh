65c14d84b1919931cf472de3f91e9d41c75a9f76
Mau <amaury@buildkite.com>
2025-01-22 18:40:40 -0500

    Updating tutorial pipeline

---
+++ src/generate-and-store-number.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function generate_number() {
+  max_number=${max_number:-9}
+  echo $(( $RANDOM % $max_number + 1 ))
+}
+
+parallel_index=${1:-}
+
+generated_number=$(generate_number)
+
+if [[ -n "$parallel_index "]]; then
+  ( set -x; buildkite-agent meta-data set "generated-numner::$parallel_index" "${generated_number}" )
+else
+  ( set -x; buildkite-agent meta-data set "generated-number" "${generated_number}" )
+fi
+
+echo
+echo
